ab9078723176a8300302f9d9595fa0088826f221
souravkairy <48748661+souravkairy@users.noreply.github.com>
2023-01-19 00:08:47 +0600

    Update header.php

--- header.php
+++ header.php
@@ -2,17 +2,39 @@
 <html lang="en">
 
 <head>
-    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Place favicon.ico in the root directory -->
     <link rel="shortcut icon" type="image/x-icon" href="<?php echo get_template_directory_uri() . '/assets/img/favicon.png' ?>">
+    <title>Sulha | Muslim Dispute Resolution Specialists</title>
+    <meta name="description" content="Sulha Solutions is a leading provider of online dispute resolution services for Muslims in Canada and across the world. We provide proficient, efficient and contingent solutions to help resolve your disputes." />
+    <link rel="canonical" href="https://sulhasolutions.com/" />
+
+    <meta property="og:locale" content="en_US" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Sulha | Muslim Dispute Resolution Specialists" />
+    <meta property="og:description" content="Sulha Solutions is a leading provider of online dispute resolution services for Muslims in Canada and across the world. We provide proficient, efficient and contingent solutions to help resolve your disputes." />
+    <meta property="og:url" content="https://sulhasolutions.com/" />
+    <meta property="og:site_name" content="Sulha" />
+    <meta property="og:updated_time" content="2022-10-02T10:49:02+00:00" />
+    <meta property="og:image" content="https://sulhasolutions.com/wp-content/uploads/2023/01/Screenshot_98-1.png" />
+    <meta property="og:image:secure_url" content="https://sulhasolutions.com/wp-content/uploads/2023/01/Screenshot_98-1.png" />
+    <meta property="og:image:width" content="1000" />
+    <meta property="og:image:height" content="628" />
+    <meta property="og:image:alt" content="Sulha" />
+    <meta property="og:image:type" content="image/jpeg" />
+    <meta property="article:published_time" content="2022-04-04T09:41:26+00:00" />
+    <meta property="article:modified_time" content="2022-10-02T10:49:02+00:00" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:title" content="Sulha | Muslim Dispute Resolution Specialists" />
+    <meta name="twitter:description" content="Sulha Solutions is a leading provider of online dispute resolution services for Muslims in Canada and across the world. We provide proficient, efficient and contingent solutions to help resolve your disputes." />
+    <meta name="twitter:image" content="https://sulhasolutions.com/wp-content/uploads/2023/01/Screenshot_98-1.png" />
+    <meta name="twitter:label1" content="Written by" />
+    <meta name="twitter:data1" content="admin" />
+    <meta name="twitter:label2" content="Time to read" />
+    <meta name="twitter:data2" content="Less than a minute" />
+
     <?php wp_head(); ?>
     <!-- All CSS -->
-
-
-    <title>sulha</title>
 </head>
 
 <body>
